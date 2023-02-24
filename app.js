4d748a8fa14562a509e4b8c5dc263a18d9fdb12e
Mathieu Favre <matfav@hotmail.fr>
2023-02-24 16:30:49 +0100

    Added sliding logic for the images

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
 const steps = document.querySelectorAll('.step')
 const left = document.querySelector('.fa-arrow-left');
 const right = document.querySelector('.fa-arrow-right');
+
 const slide1 = [
     ...document.querySelectorAll(`[data-slide="1"]`),
 ];
-console.log(slide1);
 const slide2 = [
     ...document.querySelectorAll(`[data-slide="2"]`),
 ];
-console.log(slide2);
 const slide3 = [
     ...document.querySelectorAll(`[data-slide="3"]`),
 ];
-console.log(slide3);
+
+const slides = [slide1, slide2, slide3];
+
+console.log(slides);
 
 let lastActivatedStep = 0;
 
 right.addEventListener('click', () => {
-    if (lastActivatedStep === steps.length - 1) return;
+    if (lastActivatedStep === 2) return;
     lastActivatedStep++;
     steps[lastActivatedStep].classList.toggle('active');
-    left.classList.remove('inactive-arrow')
-    if (lastActivatedStep === steps.length - 1) right.classList.add('inactive-arrow')
+    slides[lastActivatedStep - 1][4].classList.add('send-left');
+    slides[lastActivatedStep][4].classList.remove('send-right');
+    left.classList.remove('inactive-arrow');
+    if (lastActivatedStep === 2) right.classList.add('inactive-arrow');
 })
 
 left.addEventListener('click', () => {
     if (lastActivatedStep === 0) return;
     steps[lastActivatedStep].classList.toggle('active');
-    right.classList.remove('inactive-arrow')
+    slides[lastActivatedStep - 1][4].classList.remove('send-left');
+    slides[lastActivatedStep][4].classList.add('send-right');
+    right.classList.remove('inactive-arrow');
     lastActivatedStep--;
     if (lastActivatedStep === 0) return left.classList.add('inactive-arrow');
 })
